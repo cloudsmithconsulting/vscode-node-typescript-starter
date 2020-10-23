e0a0a597172a725d0be629d08e61fae3847efa26
Ira Mellor <ira.mellor@cloudsmith.consulting>
2020-10-23 11:13:56 -0400

    fixes: tests, updates packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-  };
+  root: true,
+  "env": {
+    "browser": true,
+    "amd": true,
+    "node": true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
